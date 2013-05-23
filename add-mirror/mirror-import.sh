3e0919444abfe658f74d0f6168778149714e7804
Christian Foellmann <foellmann@foe-services.de>
2013-05-24 01:11:55 +0200

    added create repo via api

--- add-mirror/mirror-import.sh
+++ add-mirror/mirror-import.sh
@@ -1,5 +1,6 @@
 # required variable
 # $plugin-slug = <the slug from the wp.org repo>
+# $plugin-svn = <repo url>
 
 # create folder + change to it
 mkdir /home/git/wp-mirrors/$plugin-slug
@@ -7,9 +8,9 @@ cd /home/git/wp-mirrors/$plugin-slug
 
 # import svn commits and remap to users from authors.txt
 # Attention!! authors.txt MUST be complete
-svn2git http://plugins.svn.wordpress.org/$plugin-slug/ --authors ~/authors.txt
+svn2git $plugin-svn --authors ~/authors.txt
 
-# add remote
+# add remote target
 git remote add github git@wp-mirrors:wp-mirrors/$plugin-slug.git
 
 # initial push to the GitHub repo
